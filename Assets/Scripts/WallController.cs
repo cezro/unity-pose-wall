5d0a8fa165048c0882bf0df32b446c830d66c0dd
cezro <hyderz1218@gmail.com>
2024-10-17 23:05:32 +0800

    test level setup

---
+++ Assets/Scripts/WallController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantVelocity : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    [SerializeField] private WallProperties properties;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        MoveWall();
+    }
+
+    void MoveWall()
+    {
+        rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, properties.speed * -1 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/WallProperties.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WallProperties : ScriptableObject
+{
+    public float speed;
+    public float holeShape;
+}
