87338a9d0d04fbc5dc327ff1f97f73c053d17cd2
cezro <hyderz1218@gmail.com>
2024-10-24 21:40:28 +0800

    concrete textures

--- Assets/Scripts/WallSpawner.cs
+++ Assets/Scripts/WallSpawner.cs
@@ -12,7 +12,7 @@ public class WallSpawner : MonoBehaviour
     private GameObject wall;
     [SerializeField] private bool isActive = true;
     [SerializeField] private float wallScale = 1f;
-    // Start is called before the first frame update
+
     void Start()
     {
         StartCoroutine(SpawnWall());
@@ -23,7 +23,7 @@ public class WallSpawner : MonoBehaviour
         while (isActive) {
             int randomIndex = UnityEngine.Random.Range(0, walls.wall.Count);
             wall = Instantiate(walls.wall[randomIndex], transform.position, Quaternion.Euler(0, 90, 0));
-            wall.transform.localScale = new Vector3(1,1.4f,wallScale);
+            wall.transform.localScale = new Vector3(1,1.4f * wallScale ,wallScale);
             if (wallProperties.frequency > 0)
             {
                 yield return new WaitForSeconds(60 / wallProperties.frequency);
